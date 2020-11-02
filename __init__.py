2df1e7a1cc00d6a5fc50c8a7e4195a180e619fcb
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2020-11-02 12:09:36 -0500

    Added couple templates that linked with routes, specifically added navigation bar using Bootstrap.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+#from requests import auth
 
+
+'''def create_app():
+  app = Flask(__name__)
+  Bootstrap(app)
+  return app 
+
+app = create_app()'''
 app = Flask(__name__)
+Bootstrap(app)
 
-@app.route('/happy')
-def welcome():
-    return 'Welcome to Jobifly!'
 
 if __name__ == "main":
     app.run()
+
+@app.route('/')
+#@app.route('/hello/<name>')
+#def hello(name=None):
+def index():
+    return render_template('home.html')
+
+@app.route('/players')
+def index():
+    return render_template('players.html')
+
