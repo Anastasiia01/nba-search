d4ab1440fc81631ce072522f58c736d0c042e2bf
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2020-11-07 10:10:18 -0500

    Started getting data from Marklogic db by adding functions to the data layer for accesing json docs. Need to correct access to images and search to work properly.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-#from requests import auth
+from . import data_layer as dl
+from base64 import b64encode
+
+#from PIL import Image
+#from io import BytesIO
 
 
 '''def create_app():
@@ -10,18 +14,33 @@ from flask_bootstrap import Bootstrap
 app = create_app()'''
 app = Flask(__name__)
 Bootstrap(app)
+dataLayer = dl.DataLayer()
 
 
 if __name__ == "main":
     app.run()
 
+
 @app.route('/')
-#@app.route('/hello/<name>')
-#def hello(name=None):
+
 def index():
-    return render_template('home.html')
+    resp = dataLayer.getBinaryDoc("/image/MalikBeasley.png")
+    #print(r.content) gives hex data
+    #pic = Image.open(BytesIO(r.content))
+    image = b64encode(resp).decode("utf-8")
+    #print(image)
+
+    return render_template('home.html', pic=pic)
 
 @app.route('/players')
-def players():
-    return render_template('players_list.html')
 
+def players():
+    player = dataLayer.searchJsonDoc("2018")
+    print(player)
+    """curl --anyauth --user user:password -X GET -i \
+    -H "Accept: multipart/mixed; boundary=document-part-boundary" \
+    'tp://localhost:8000/v1/documents?uri=doc1.xml&uri=doc2.json' """
+    return render_template('players_list.html', players = player)
+
+#@app.route('/hello/<name>')
+#def hello(name=None):
--- data_layer.py
+++ data_layer.py
@@ -1,8 +1,30 @@
 from urllib.parse import urljoin
-from urllib import urlencode
+from flask import jsonify
+#from urllib import urlencode
 import requests
 
 class DataLayer():
-    self.auth = requests.auth.HTTPDigestAuth("admin","admin")
-    self.host = 'http://localhost:8070/v1/'
-    self.endpoints = ['documents', 'search']
+    auth = requests.auth.HTTPDigestAuth("admin","admin")
+    host = 'http://localhost:8055/v1/'
+    endpoints = ['documents?uri=', 'search?q=']
+
+    def getJsonDoc(self, uri):
+        link = "{}{}{}".format(self.host, self.endpoints[0], uri)
+        resp = requests.get(link, auth = self.auth)
+        return resp.json()
+
+    def getBinaryDoc(self, uri):
+        link = "{}{}{}".format(self.host, self.endpoints[0], uri)
+        resp = requests.get(link, auth = self.auth)
+        return resp
+
+    def searchJsonDoc(self, query):
+        link = "{}{}{}&format=json".format(self.host, self.endpoints[1], query)
+        #print("link: ", link)
+        resp = requests.get(link, auth = self.auth)
+        print(resp)
+        return resp.json()['results']
+
+
+
+#ttp://localhost:8055/v1/documents?uri=/image/MalikBeasley.png
